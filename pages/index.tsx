ad6ffd5ff7f744b6248a5cc15d70706cb905c412
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-10 18:26:20 +0200

    scaffolding

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function Register() {
+  return (
+    <div>
+      Hello there!{" "}
+      <span role="img" aria-labelledby="alarm">
+        🚨
+      </span>{" "}
+    </div>
+  );
+}
+
+export default Register;
