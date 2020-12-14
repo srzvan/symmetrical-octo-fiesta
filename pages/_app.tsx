69ce04902bcf4c4fa645e0f5137142f84c63a6e3
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-14 14:31:54 +0200

    <SplashScreen /> & global styles

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import CSSBaseline from "@material-ui/core/CssBaseline";
+
+import SplashScreen from "../src/components/splash-screen";
+
+import "../src/global-styles.css";
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <CSSBaseline />
+      <SplashScreen>
+        <Component {...pageProps} />
+      </SplashScreen>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/splash-screen.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Fade, Grid, makeStyles, Typography } from "@material-ui/core";
+
+const { useEffect, useState } = React;
+
+const useStyles = makeStyles({
+  loading: {
+    height: "100%",
+    background: "linear-gradient(114.83deg, #3EC1BE 1.18%, #006EAD 84.18%)",
+  },
+});
+
+function Loading(props: { className?: string }) {
+  return (
+    <Grid container alignItems="center" justify="center" {...props}>
+      <Typography component="div" align="center">
+        <h1>Symmetrical</h1>
+        <h2>Octo</h2>
+        <h3>Fiesta</h3>
+      </Typography>
+    </Grid>
+  );
+}
+
+function SplashScreen({ children }) {
+  var [isLoading, setIsLoading] = useState(true);
+
+  var classes = useStyles();
+
+  useEffect(() => {
+    mimicAsyncWork();
+
+    function mimicAsyncWork() {
+      try {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, []);
+
+  return isLoading ? <Loading className={classes.loading} /> : children;
+}
+
+export default SplashScreen;
