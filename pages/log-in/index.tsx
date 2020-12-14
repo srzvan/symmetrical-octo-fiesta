8a42d1144d979840558ccf0c6a93a528a9a1bc30
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-14 14:37:03 +0200

    dummy login page

---
+++ pages/log-in/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@material-ui/core";
+import * as React from "react";
+
+function LogIn() {
+  return (
+    <Typography>
+      Login not yet implemented{" "}
+      <span role="img" aria-labelledby="alarm">
+        🚨
+      </span>
+    </Typography>
+  );
+}
+
+export default LogIn;
