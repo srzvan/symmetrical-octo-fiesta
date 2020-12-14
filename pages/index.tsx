e03c84c68b64545e6e117b3245b464e49ffbe48f
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-14 18:26:54 +0200

    refactor register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,45 @@
 import * as React from "react";
+import { Button, Container, makeStyles, Typography } from "@material-ui/core";
+
+import RegisterForm from "../src/components/RegisterForm";
+
+const useStyles = makeStyles({
+  container: {
+    color: "white",
+    background: "linear-gradient(270deg, #0AA5AB 0.4%, #00528C 99.98%)",
+    height: "100%",
+  },
+  title: {
+    fontSize: "2rem",
+    fontWeight: 700,
+  },
+  alreadyHaveAnAccount: {
+    marginTop: "2.375rem",
+    "& > p": {
+      marginBottom: "0.75rem",
+    },
+    "& a": {
+      padding: 0,
+    },
+  },
+});
 
 function Register() {
+  var classes = useStyles();
+
   return (
-    <div>
-      Hello there!{" "}
-      <span role="img" aria-labelledby="alarm">
-        🚨
-      </span>{" "}
-    </div>
+    <Container className={classes.container}>
+      <Typography className={classes.title} component="h1" paragraph>
+        Register
+      </Typography>
+
+      <RegisterForm />
+
+      <Typography className={classes.alreadyHaveAnAccount} component="div" align="center">
+        <p>Already have an account?</p>
+        <Button href="/log-in" children="log in" />
+      </Typography>
+    </Container>
   );
 }
 
