2d79e7b6571440a83bdbe24ad6db9a7587ee5aff
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-15 14:18:12 +0200

    capitalized component name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import CSSBaseline from "@material-ui/core/CssBaseline";
 
-import SplashScreen from "../src/components/splash-screen";
+import SplashScreen from "../src/components/SplashScreen";
 
 import "../src/global-styles.css";
 
