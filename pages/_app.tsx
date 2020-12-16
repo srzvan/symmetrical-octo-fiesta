a74ac11c03aa654ca9cd2c4356abda5bf7cc8e46
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-16 18:06:25 +0200

    theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
 import * as React from "react";
-import CSSBaseline from "@material-ui/core/CssBaseline";
+import Head from "next/head";
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from "@material-ui/core";
 
 import SplashScreen from "../src/components/SplashScreen";
 
+import { theme as themeObject } from "../src/theme";
+
 import "../src/global-styles.css";
+
+var theme = responsiveFontSizes(createMuiTheme(themeObject));
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <CSSBaseline />
+      <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
       <SplashScreen>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </SplashScreen>
     </>
   );
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { ThemeOptions } from "@material-ui/core";
+import { PaletteOptions } from "@material-ui/core/styles/createPalette";
+
+var palette: PaletteOptions = {
+  background: {
+    default: "#fff",
+  },
+};
+
+export var theme: ThemeOptions = {
+  palette,
+};
