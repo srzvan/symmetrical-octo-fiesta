68a809f80fba67bac77809d2ecb34ef8f623fa7c
Răzvan Sbîngu <sbingu.razvan@gmail.com>
2020-12-16 18:07:04 +0200

    header clip-path & removed unused global css prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,24 @@ import { Button, Container, Grid, Hidden, makeStyles, Theme, Typography } from "
 
 import RegisterForm from "../src/components/RegisterForm";
 
-const useStyles = makeStyles((theme: Theme) => ({
+var useStyles = makeStyles((theme: Theme) => ({
   header: {
+    padding: "40px 16px 34px 16px",
     height: "130px",
     textAlign: "center",
+    backgroundColor: "transparent",
   },
   grid: {
+    clipPath: "polygon(0 0, 100% 30px, 100% 100%, 0% 100%)",
     height: "100%",
   },
   leftColumn: {
+    paddingTop: "20px",
     background: "url(./1e0c95c222d46d253d2449723cbc0f87.jpg) top center no-repeat",
     backgroundSize: "cover",
   },
   rightColumn: {
+    paddingTop: "20px",
     color: "white",
     background: "linear-gradient(270deg, #0AA5AB 0.4%, #00528C 99.98%)",
   },
@@ -43,7 +48,7 @@ function Register() {
   return (
     <>
       <header className={classes.header}>
-        <Typography variant="h1">Header</Typography>
+        <Typography variant="body1">Header</Typography>
       </header>
       <Grid container className={classes.grid}>
         <Hidden mdDown>
